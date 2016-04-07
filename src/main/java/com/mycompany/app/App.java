8a8b18ec9e1b0132c7b3ab2b799c7702abfe6005
Nigel Magnay <nigel.magnay@gmail.com>
2016-04-07 14:15:17 +0100

    bug?

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        // Generate some findbugs perhaps
+        int j;
+
+        System.out.println("A" + "B" + "C");
+
     }
 }
